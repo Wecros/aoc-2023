922d17234c97519be3850125c1301d4766c51c6c
Marek Filip (Wecros) <wecros@gmail.com>
2023-12-10 10:01:26 +0100

    Add day 9 solution

---
+++ day9/part1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+
+def main():
+    history_lines = [[int(num) for num in line.split()] for line in fileinput.input()]
+    prediction_sum = sum(predict(line) for line in history_lines)
+    print(prediction_sum)
+
+
+def predict(history_line):
+    """Predict the next number in line."""
+    all_lines = [history_line]
+    last_line = history_line
+
+    # compute until last line is all zeros
+    while True:
+        next_line = compute_next_line(last_line)
+        if all(num == 0 for num in next_line):
+            break
+        all_lines.append(next_line)
+        last_line = next_line
+
+    predicted_number = 0
+    for line in all_lines[::-1]:
+        predicted_number = predicted_number + line[-1]
+
+    return predicted_number
+
+
+def compute_next_line(last_line):
+    next_line = []
+    for i in range(1, len(last_line)):
+        next_line.append(last_line[i] - last_line[i - 1])
+
+    return next_line
+
+if __name__ == "__main__":
+    main()
